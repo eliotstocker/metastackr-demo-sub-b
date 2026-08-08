7f8641945d778c2d6ab94004caeb202b328e6908
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 21:32:37 +0100

    feat: v17 single comment and status test

--- service-b.go
+++ service-b.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from Service B - v16-single-comment-test")
+	fmt.Println("Hello from Service B - v17-single-comment-and-status-test")
 }
